faa65d8e2d40c4ad24feaa8af440c8ed79d5a612
Anderson Fagundes Martins <anderson.fagundes@gmail.com>
2020-06-17 23:11:25 -0300

    Adiciona o módulo externo lacuisine_gsoap_common
     O módulo lacuisine_gsoap_common é utilizado para gerar o código fonte para suporte ao
    protocolo gSOAP ao projeto LaCuisine.

---
+++ src/gsoap/lacuisineBindingServiceImpl.cpp
@@ -0,0 +1,5 @@
+
+#include "soapLacuisineBindingService.h"
+#include "LacuisineBinding.nsmap"
+
+int LacuisineBindingService::AccessAuthentication(ns1__AccessAuthenticationRequestType */*ns1__AccessAuthenticationRequest*/, ns1__AccessAuthenticationResponseType &/*ns1__AccessAuthenticationResponse*/) {return SOAP_OK;}
