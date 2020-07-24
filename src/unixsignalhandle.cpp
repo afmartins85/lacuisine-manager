2c93b7f4cee77019c1010690657584110c3f9666
Anderson Fagundes Martins <anderson.fagundes@truetechsystems.com.br>
2020-07-24 09:16:01 -0300

    Removido do código de manipulação de sinais Unix, a thread criada para tentar resolver a questão do monitoramento do trigger dos sinais.
    Essa implementação não apresentou resultados satisfatórios e, portanto, foi removido do código.

--- src/unixsignalhandle.cpp
+++ src/unixsignalhandle.cpp
@@ -2,15 +2,13 @@
 #include "loguru.hpp"
 #include <QObject>
 
-bool g_isSigTrigged = false;
 QMap<int, QString> UnixSignalHandle::sigMap{{1, "SIGHUP"},   {2, "SIGINT"},   {3, "SIGQUIT"},  {4, "SIGABRT"},
                                             {9, "SIGKILL"},  {10, "SIGBUS"},  {11, "SIGSEGV"}, {12, "SIGSYS"},
                                             {13, "SIGPIPE"}, {14, "SIGALRM"}, {15, "SIGTERM"}, {17, "SIGSTOP"}};
 
-UnixSignalHandle::UnixSignalHandle(QObject *parent) : QThread(parent) {
+UnixSignalHandle::UnixSignalHandle(QObject *parent) : QObject(parent) {
   catchUnixSignals({SIGQUIT, SIGINT, SIGTERM, SIGSEGV, SIGHUP});
-  connect(this, &UnixSignalHandle::finished, this, &UnixSignalHandle::deleteLater);
-  this->start();
+  //  connect(this, &UnixSignalHandle::finished, this, &UnixSignalHandle::deleteLater);
 }
 
 /**
@@ -31,9 +29,6 @@ void UnixSignalHandle::catchUnixSignals(std::initializer_list<int> quitSignals) 
     QString sigString = sigMap.find(sig).value();
     // blocking and not aysnc-signal-safe func are valid
     LOG_F(INFO, "quit the application by signal(%d)(%s).\n", sig, sigString.toLatin1().data());
-    g_isSigTrigged = true;
-    while (1)
-      ;
   };
 
   sigset_t blocking_mask;
@@ -46,17 +41,4 @@ void UnixSignalHandle::catchUnixSignals(std::initializer_list<int> quitSignals) 
   sa.sa_flags = 0;
 
   for (auto sig : quitSignals) sigaction(sig, &sa, nullptr);
-}
-
-void UnixSignalHandle::trigger(int sig) {
-  QString sigString = sigMap.find(sig).value();
-  // blocking and not aysnc-signal-safe func are valid
-  LOG_F(INFO, "quit the application by signal(%d)(%s).\n", sig, sigString.toLatin1().data());
-  emit sigTrigged(sig);
-}
-
-void UnixSignalHandle::run() {
-  if (g_isSigTrigged == true) {
-    LOG_F(WARNING, "UNIX SIGNAL RECEIVED!!!");
-  }
 }
